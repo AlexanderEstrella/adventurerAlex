00dfd975b6040f049c53a5358ccea019287649f8
alexanderestrella23 <Alexander>
2023-02-25 15:07:33 -0500

    added first trip image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/header";
 import Home from "./Components/home";
+import Places from "./Components/places";
 function App() {
   return (
     <div>
       <Header />
       <Home />
+       <Places />
     </div>
   );
 }
---
+++ src/Components/places.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Places = () => {
+    return (
+<div className="places">
+<h2> places </h2>
+<div className="placesholdings">
+<img alt="costarica" src="https://media.istockphoto.com/id/153824056/photo/arenal-volcano-in-wispy-clouds.jpg?s=612x612&w=0&k=20&c=oeStaKW4pyZ3o0H7XBFZ0w7DC34cA5AGv_hoFJ70e-U=" />
+<img alt="mexico" />
+<img alt="dominicanrepublic"/>
+</div>
+</div>
+    );
+};
+
+export default Places;
